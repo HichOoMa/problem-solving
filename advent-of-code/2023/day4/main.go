f593000507418aeca88d43a29a707126a1e8a648
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-06 22:26:54 +0100

    advent of code day4 part 1

---
+++ advent-of-code/2023/day4/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	pkg "github.com/HaithamBH/problem-solving/advent-of-code"
+)
+
+func main() {
+	lines := pkg.FileIntoLines("/home/hichoma/Dev/problem-solving/advent-of-code/2023/day4/puzzle.txt")
+
+	part1(lines)
+}
+
+func part1(lines []string) {
+	count := 0
+	for _, line := range lines {
+		data := strings.Split(line, ":")
+		isPlayNum := false
+		index := 0
+		playNums := []int{}
+		winNums := []int{}
+		for index < len(data[1]) {
+			char := data[1][index]
+			if string(char) == "|" {
+				isPlayNum = true
+				index++
+				continue
+			} else if string(char) == " " {
+				index++
+				continue
+			} else {
+				var num int
+				if index < len(data[1])-1 && string(data[1][index+1]) == " " || index == len(data[1])-1 {
+					num, _ = strconv.Atoi(string(data[1][index]))
+				} else {
+					num, _ = strconv.Atoi(data[1][index : index+2])
+					index++
+				}
+				if isPlayNum {
+					playNums = append(playNums, num)
+				} else {
+					winNums = append(winNums, num)
+				}
+			}
+			index++
+		}
+		points := 0
+		for _, pNum := range playNums {
+			if slices.Contains(winNums, pNum) {
+				if points == 0 {
+					points = 1
+				} else {
+					points = points * 2
+				}
+			}
+		}
+		count = count + points
+	}
+	fmt.Print(count)
+}
