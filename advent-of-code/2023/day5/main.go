b60a866e360ccd72d3aa16b4f74268cc50567b9d
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-08 20:55:18 +0100

    advent of code day 5 part2

--- advent-of-code/2023/day5/main.go
+++ advent-of-code/2023/day5/main.go
@@ -13,7 +13,8 @@ func main() {
 		"/home/hichoma/Dev/problem-solving/advent-of-code/2023/day5/puzzle.txt",
 	)
 
-	part1(lines)
+	// part1(lines)
+	part2(lines)
 }
 
 type rangeMap struct {
@@ -83,6 +84,86 @@ func part1(lines []string) {
 		}
 	}
 	fmt.Print(min)
+}
+
+func part2(lines []string) {
+	seeds := strings.Split(lines[0], ":")
+
+	trackList := []int{}
+	minList := []int{}
+	charIndex := 1
+	for charIndex < len(seeds[1]) {
+		if string(seeds[1][charIndex]) != " " {
+			seed, lastIndex := getFullNumber(seeds[1], charIndex)
+			// trackList = append(trackList, seed)
+			charIndex = lastIndex + 1
+			seed2, lastIndex2 := getFullNumber(seeds[1], charIndex)
+			charIndex = lastIndex2
+			for i := seed; i <= seed+seed2-1; i++ {
+				trackList = append(trackList, i)
+			}
+			minimum := getMinLocation(lines, trackList)
+			trackList = []int{}
+			minList = append(minList, minimum)
+			continue
+		}
+		charIndex++
+	}
+	minLoc := 9223372036854775807
+	for _, val := range minList {
+		if val < minLoc {
+			minLoc = val
+		}
+	}
+	println(minLoc)
+}
+
+func getMinLocation(lines []string, trackList []int) int {
+	mapNum := 1
+	for _, line := range lines[2:] {
+		if line == "" {
+			for trackedIndex, tracked := range trackList {
+				for _, rg := range rgMap {
+					if tracked >= rg.startIndex && tracked <= rg.lastIndex {
+						trackList[trackedIndex] = rg.startRange + tracked - rg.startIndex
+					}
+				}
+			}
+			mapNum++
+			rgMap = []rangeMap{}
+			continue
+		}
+		if _, b := isInt(string(line[0])); !b {
+			continue
+		}
+		index := 0
+		var numList []int
+		for index < len(line) {
+			char := line[index]
+			if _, b := isInt(string(char)); b {
+				num, newIndex := getFullNumber(line, index)
+				index = newIndex
+				numList = append(numList, num)
+				continue
+			}
+			index++
+		}
+		rgMap = append(rgMap, rangeMap{numList[0], numList[1], numList[1] + numList[2] - 1})
+	}
+	for trackedIndex, tracked := range trackList {
+		for _, rg := range rgMap {
+			if tracked >= rg.startIndex && tracked <= rg.lastIndex {
+				trackList[trackedIndex] = rg.startRange + tracked - rg.startIndex
+			}
+		}
+	}
+	min := 9223372036854775807
+	for _, tracked := range trackList {
+		if tracked < min {
+			min = tracked
+		}
+	}
+	return min
 }
 
 func isInt(str string) (int, bool) {
