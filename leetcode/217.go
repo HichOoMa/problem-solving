c39a6fc6fd4bee1ec2e54339bf6f0ca8b2ea87f7
HaithamBH <haitham.benhammouda@gmail.com>
2024-05-02 07:19:05 +0100

    leetcode 217

---
+++ leetcode/217.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import (
+	"slices"
+	"sort"
+)
+
+type SortBy []int
+
+func (a SortBy) Len() int           { return len(a) }
+func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortBy) Less(i, j int) bool { return a[i] < a[j] }
+
+func containsDuplicate(nums []int) bool {
+	sort.Sort(SortBy(nums))
+	for {
+		if len(nums) == 1 {
+			return false
+		}
+		if _, b := slices.BinarySearch(nums[1:], nums[0]); b {
+			return true
+		} else {
+			nums = nums[1:]
+		}
+	}
+}
---
+++ leetcode/217_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_containsDuplicate(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"test 1", args{[]int{1, 2, 3, 1}}, true},
+		{"test 2", args{[]int{1, 2, 3, 4}}, false},
+		{"test 3", args{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := containsDuplicate(tt.args.nums); got != tt.want {
+				t.Errorf("containsDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
